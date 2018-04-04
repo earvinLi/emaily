b0cfb103c830d392f3ed52b0b7021f087e8d069c
Earvin Li <earvin.tli@gmail.com>
2018-04-04 18:30:20 -0500

    [App] Build auth and callback

--- index.js
+++ index.js
@@ -9,12 +9,21 @@ passport.use(
   new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback'
   },(accessToken) => {
     console.log(accessToken);
   })
 );
 
-const PORT = process.env.PORT || 5000;
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT);
