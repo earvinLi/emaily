d130e7382a6931826e20a189a11fe6028a2b1cc6
Earvin Li <earvin.tli@gmail.com>
2018-05-12 16:53:10 -0500

    [App] Refactor promise by async and await

--- services/passport.js
+++ services/passport.js
@@ -1,8 +1,10 @@
-//
+//External Dependencies
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
 const mongoose = require('mongoose');
 const passport = require('passport');
+
+// Local Dependencies
+const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
@@ -23,16 +25,14 @@ passport.use(
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true
-  },(accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleID: profile.id })
-      .then((existingUser) => {
-        if (existingUser) {
-          done(null, existingUser);
-        }
-        else { new User ({ googleID: profile.id })
-          .save()
-          .then(newUser => done(null, newUser));
-        }
-      });
-  })
-);
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({ googleID: profile.id });
+    
+    if (existingUser) {
+      return done(null, existingUser);
+    }
+    const newUser = await new User ({ googleID: profile.id }).save();
+    done(null, newUser);
+  }
+));
